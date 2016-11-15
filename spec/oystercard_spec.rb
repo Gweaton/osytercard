36af9929b650894961f8829b1daa19714158e394
George Eaton <george_eaton@hotmail.co.uk>
2016-11-15 11:25:16 +0000

    added top_up method to top up balance on oyster

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,17 @@ describe Oystercard do
       expect(oyster.balance).to eq 0
     end
   end
+
+  describe "#top_up" do
+    it 'should have a top_up method' do
+      expect(oyster).to respond_to(:top_up).with(1).argument
+    end
+    it 'should add to the current balance' do
+      expect(oyster.top_up(10)).to eq 10
+    end
+    it 'should show the topped up balance' do
+      oyster.top_up(10)
+      expect(oyster.balance).to eq 10
+    end
+  end
 end

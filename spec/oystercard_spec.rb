17e60c280796a39fa5ed84f6faf2471ce474348f
George Eaton <george_eaton@hotmail.co.uk>
2016-11-15 10:59:51 +0000

    added default balance of 0 to new oysters

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
 require 'oystercard'
-describe Oystercard do
 
+describe Oystercard do
+  subject(:oyster) {described_class.new}
+
+  context "new card" do
+    it 'has an initial balance of 0' do
+      expect(oyster.balance).to eq 0
+    end
+  end
 end
